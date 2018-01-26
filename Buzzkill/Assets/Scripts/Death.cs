e9e19921866909ca14f0935162491bf7ecfa7bbf
adamt <adatuchb@uat.edu>
2018-01-26 12:41:29 -0700

    newest set update

---
+++ Buzzkill/Assets/Scripts/Death.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+	void OnTriggerEnter(Collider other) {
+		Destroy (other.gameObject);
+	}
+
+}
---
+++ Buzzkill/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+
+	public float maxSpeed = -5;
+	public Vector3 userDirection = Vector3.left;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (userDirection * maxSpeed * Time.deltaTime);
+		
+	}
+}
