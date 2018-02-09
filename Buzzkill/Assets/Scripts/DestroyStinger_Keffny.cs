bed287182e725a9adba5b6355573cae8fc8df379
KNOWN-UNIVERSE\kefcharl <keffnycharles@gmail.com>
2018-02-09 16:14:00 -0700

    Added the stinger projectile

---
+++ Buzzkill/Assets/Scripts/DestroyStinger_Keffny.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyStinger_Keffny : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Destroy(gameObject, 2);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Destroy(other.gameObject);
+		Destroy(this.gameObject);
+	}
+}
