e1d4daade0edb4b4f7765232f78c8ac145e5bbf5
KNOWN-UNIVERSE\nikserop <nikserop>
2018-02-13 16:14:42 -0700

    Melee attack

---
+++ Buzzkill/Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour {
+	public int score;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		//Debug.Log ("Collision detected");
+		if (other.gameObject.tag == "Enemy") {
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ Buzzkill/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestruct : MonoBehaviour {
+
+	//Makes bullets dissapear after awhile
+	public float timer = .1f;
+
+	void Update () {
+		timer -= Time.deltaTime;
+
+		if(timer <= 0) {
+			Destroy(gameObject);
+		}
+	}
+}
+
