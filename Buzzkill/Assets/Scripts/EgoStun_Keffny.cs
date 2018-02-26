6870c0f485f3bcc75cb3cabc2eba6a419bc41a52
DESKTOP-OU1TFH4\Keffny <keffnycharles@gmail.com>
2018-02-25 23:45:43 -0700

    Ego Stun Sound

---
+++ Buzzkill/Assets/Scripts/EgoStun_Keffny.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EgoStun_Keffny : MonoBehaviour
+{
+	public AudioClip stun;
+
+	private AudioSource soundEffect;
+
+	// Use this for initialization
+	void Start ()
+	{
+		soundEffect = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.name == "projectile_hotdog(Clone)")
+		{
+			soundEffect.PlayOneShot(stun);
+		}
+	}
+}
