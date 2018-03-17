6758f6e6372a5b784c3e9d590d9588e911f69022
KNOWN-UNIVERSE\kefcharl <keffnycharles@gmail.com>
2018-03-16 17:29:36 -0700

    Audio Menu(Not Done) and Scene Changes between menus

--- Buzzkill/Assets/Scripts/MenuManager.cs
+++ Buzzkill/Assets/Scripts/MenuManager.cs
@@ -2,19 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour {
 
 	public GameObject pauseMenu;
 	public GameObject pauseButton;
 
+	public Slider masterVolume;
+	public Slider musicVolume;
+	public Slider effectsVolume;
+
+	//public float startVolume = 10f;
+	public float masterCurrentVolume;
+	public float musicCurrentVolume;
+	public float effectsCurrentVolume;
+	string newMasterVolume = "MASTER_VOLUME_SLIDER";
+	string newMusicVolume = "MUSIC_VOLUME_SLIDER";
+	string newEffectsVolume = "EFFECTS_VOLUME_SLIDER";
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		masterVolume.value = PlayerPrefs.GetFloat(newMasterVolume, 10);
+		musicVolume.value = PlayerPrefs.GetFloat(newMusicVolume, 10);
+		effectsVolume.value = PlayerPrefs.GetFloat(newEffectsVolume, 10);
+		masterCurrentVolume = masterVolume.value;
+		musicCurrentVolume = musicVolume.value;
+		effectsCurrentVolume = effectsVolume.value;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
+		//MasterVolumeControl();
+		//MusicVolumeControl();
+		//EffectsVolumeControl();
 	}
 
 	public void StartGame(){
@@ -35,6 +58,82 @@ public class MenuManager : MonoBehaviour {
 		SceneManager.LoadScene (0);
 	}
 
+	public void OptionMenu()
+	{
+		SceneManager.LoadScene("Options");
+	}
+
+	public void AudioMenu()
+	{
+		SceneManager.LoadScene("Audio");
+	}
+
+	public void MasterVolumeControl(float volume1)
+	{
+		masterCurrentVolume = volume1;
+		PlayerPrefs.SetFloat(newMasterVolume, volume1);
+		PlayerPrefs.Save();
+	}
+
+	public void MusicVolumeControl(float volume2)
+	{
+		musicCurrentVolume = volume2;
+		PlayerPrefs.SetFloat(newMusicVolume, volume2);
+		PlayerPrefs.Save();
+	}
+
+	public void EffectsVolumeControl(float volume3)
+	{
+		effectsCurrentVolume = volume3;
+		PlayerPrefs.SetFloat(newEffectsVolume, volume3);
+		PlayerPrefs.Save();
+	}
+
+	void OnEnable()
+	{
+		masterVolume.onValueChanged.AddListener(delegate{SliderCalled(masterVolume);});
+		musicVolume.onValueChanged.AddListener(delegate{SliderCalled(musicVolume);});
+		effectsVolume.onValueChanged.AddListener(delegate{SliderCalled(effectsVolume);});
+	}
+
+	void SliderCalled(Slider sliderMoved)
+	{
+		if(sliderMoved == masterVolume)
+		{
+			MasterVolumeControl(sliderMoved.value);
+		}
+
+		if(sliderMoved == musicVolume)
+		{
+			MusicVolumeControl(sliderMoved.value);
+		}
+
+		if(sliderMoved == effectsVolume)
+		{
+			EffectsVolumeControl(sliderMoved.value);
+		}
+	}
+
+	public void SoundtrackMenu()
+	{
+		SceneManager.LoadScene("");
+	}
+
+	public void AestheticMenu()
+	{
+		SceneManager.LoadScene("Aesthetics");
+	}
+
+	public void BackFromOptionMenu()
+	{
+		SceneManager.LoadScene("StartScreen");
+	}
+
+	public void BackToOptionMenu()
+	{
+		SceneManager.LoadScene("Options");
+	}
+		
 	public void Pause(){
 		GameManager.instance.Pause ();
 		pauseMenu.SetActive (!pauseMenu.activeSelf);
